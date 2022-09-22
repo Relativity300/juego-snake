34bcdbfe27a4c268b23b9baee40ca9a9b4b6062f
Johan Rojas <johanandresrojasnaranjo@gmail.com>
2022-09-22 02:44:44 -0500

    se creo la list de top

--- js/localstorage.js
+++ js/localstorage.js
@@ -11,8 +11,16 @@ $(document).ready(function(){
     var txt_user = localStorage.getItem('usuario');
     var txt_password =localStorage.getItem('password');
     
+    let nombre0="juan";
+    let nombre1="johan";
+    let nombre2="diego";
+    let nombre3="pedro";
+    let nombre4="laura";
+    let nombre5="andres";
+    let nombre6="pepe";
+    let nombre7="lolaloca";
 
-    if(txt_user == "juan" && txt_password == '123' || txt_user == "johan" && txt_password == '123' || txt_user == "diego" && txt_password == '123' ){
+    if(txt_user == nombre0 && txt_password == '123' || txt_user == nombre1 && txt_password == '123' || txt_user == nombre2 && txt_password == '123' || txt_user == nombre3 && txt_password == '123'|| txt_user == nombre4 && txt_password == '123'|| txt_user == nombre5 && txt_password == '123'|| txt_user == nombre6 && txt_password == '123'|| txt_user == nombre7 && txt_password == '123' ){
         //llamamos al documento sesion.html
         localStorage.setItem("p", 0);
 
